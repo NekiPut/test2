d7fc5bb888e213e5f9d806d1a2810e5ab6c954b1
Neki.Nikolas <WasWillstDuTun@kek.com>
2021-12-01 09:02:33 +0100

    fix

---
+++ TestProject/src/test/World.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Random;
+
+public class World {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		System.out.println("How are you doin ?");
+
+		Random rdm = new Random();
+		int zufallszahl = rdm.nextInt(100);	
+		System.out.println(zufallszahl);
+	}
+
+}

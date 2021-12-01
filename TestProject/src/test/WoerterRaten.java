56903178087e69a0362ca44edccc68a425874778
Neki.Nikolas <WasWillstDuTun@kek.com>
2021-12-01 09:27:32 +0100

    Collection Shuffle String Characters

--- TestProject/src/test/WoerterRaten.java
+++ TestProject/src/test/WoerterRaten.java
@@ -1,13 +1,22 @@
 package test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class WoerterRaten {
 
 	public static void main(String[] args) {
 
 		String wort = "BICYCLE";
-		System.out.println(wort.charAt(3));
+		List<String> characters = Arrays.asList(wort.split(""));
+		Collections.shuffle(characters);
+		String afterShuffle = "";
+		for(String character : characters) {
 		
-		
+		afterShuffle += character;
+		}
+		System.out.println(afterShuffle);
 	}
 
 }

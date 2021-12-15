c4ef58845d7f0cd8c556cbef6b519f3f33ae3977
Neki.Nikolas <WasWillstDuTun@kek.com>
2021-12-15 09:27:47 +0100

    Voll krasse For schleife Prüfung auf die buchstaben

--- TestProject/src/test/WoerterRatenDrwenskiMethod.java
+++ TestProject/src/test/WoerterRatenDrwenskiMethod.java
@@ -28,10 +28,13 @@ public class WoerterRatenDrwenskiMethod {
 		System.out.println("Dein Tipp: ");
 		String tipp = eingabe.next();
 
-		if(tipp.charAt(0) == wort.charAt(0)) {
-			System.out.println("1. Buchstabe richtig!");
-		}else {
-			System.out.println("1. Buchstabe falsch! baka");
+		for(int i = 0; i < wort.length(); i++) {
+		
+			if(tipp.charAt(i) == wort.charAt(i)) {
+				System.out.println((i + 1) + ". Buchstabe richtig!");
+			}else {
+				System.out.println((i + 1) + ". Buchstabe falsch! baka");
+			}	
 		}
 		
 //		if(tipp.equalsIgnoreCase(wort)) {
